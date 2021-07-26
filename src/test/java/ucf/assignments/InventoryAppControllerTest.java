a27306e9bfa54b71833aa9fc784d45e11b49c2d3
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-07-25 23:12:18 -0400

    update

--- src/test/java/ucf/assignments/InventoryAppControllerTest.java
+++ src/test/java/ucf/assignments/InventoryAppControllerTest.java
@@ -1,5 +1,10 @@
 package ucf.assignments;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 5 Solution
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
